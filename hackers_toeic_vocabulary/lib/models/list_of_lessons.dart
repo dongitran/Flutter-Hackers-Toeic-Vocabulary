61796914a9ff7370f7fde8eecf9f6bbcb05761e4
dongtran <thiendong.iuh@gmail.com>
2023-10-31 23:51:04 +0700

    update image

--- hackers_toeic_vocabulary/lib/models/list_of_lessons.dart
+++ hackers_toeic_vocabulary/lib/models/list_of_lessons.dart
@@ -16,47 +16,47 @@ List<LessionModel> listOfLessons = [
   LessionModel(
     title: "Thoát cảnh thất nghiệp",
     location: "Day 1",
-    image: "assets/images/paris.jpg",
+    image: "assets/images/day1.jpg",
     tag: 1,
   ),
   LessionModel(
     title: "Quy định về trang phục",
     location: "Day 2",
-    image: "assets/images/images.jpeg",
+    image: "assets/images/day2.jpg",
     tag: 2,
   ),
   LessionModel(
     title: "Cao thủ chốn văn phòng",
     location: "Day 3",
-    image: "assets/images/Sossusvlei.jpg",
+    image: "assets/images/day3.jpg",
     tag: 3,
   ),
   LessionModel(
     title: "Bí quyết kinh doanh",
     location: "Day 4",
     image:
-        "assets/images/22bab5ad4b9aa1027ad00a84ea7493d2c0c5e666d43d3b9413e332bdbd3f1780.jpg",
+        "assets/images/day4.jpg",
     tag: 4,
   ),
   LessionModel(
     title: "Vũ khí bí mật",
     location: "Day 5",
     image:
-        "assets/images/607d0368488549e7b9179724b0db4940.jpg",
+        "assets/images/day5.jpg",
     tag: 5,
   ),
   LessionModel(
     title: "Ngày nghỉ",
     location: "Day 6",
     image:
-        "assets/images/607d0368488549e7b9179724b0db4940.jpg",
+        "assets/images/day6.jpg",
     tag: 6,
   ),
   LessionModel(
     title: "Chiến lược marketing",
     location: "Day 7",
     image:
-        "assets/images/607d0368488549e7b9179724b0db4940.jpg",
-    tag: 5,
+        "assets/images/day7.jpg",
+    tag: 7,
   ),
 ];
